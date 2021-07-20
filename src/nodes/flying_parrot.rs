ee9fdc56460f01e010e0954731953a0f03947748
Saverio Miroddi <saverio.pub2@gmail.com>
2021-07-20 23:42:28 +0200

    Parrot: Change motion to sinusoidal

--- src/nodes/flying_parrot.rs
+++ src/nodes/flying_parrot.rs
@@ -17,13 +17,18 @@ const FLYING_PARROT_COUNTDOWN_DURATION: f32 = 0.75;
 const FLYING_PARROT_WIDTH: f32 = 40.;
 pub const FLYING_PARROT_HEIGHT: f32 = 18.;
 const FLYING_PARROT_ANIMATION_FLYING: &'static str = "flying";
-const FLYING_PARROT_INITIAL_SPEED_X_REL: f32 = 600.;
-const FLYING_PARROT_INITIAL_SPEED_Y: f32 = -200.;
+const FLYING_PARROT_SPEED_X: f32 = 600.;
+const FLYING_PARROT_MAX_AMPLITUDE: f32 = 100.;
+const FLYING_PARROT_Y_FREQ_SLOWDOWN: f32 = 75.; // the higher, the slower the frequency is
 const FLYING_PARROT_MOUNT_X_REL: f32 = -35.;
 
+/// The FlyingParrot doesn't have a body, as it has a non-conventional (sinuisodal) motion.
 pub struct FlyingParrot {
     flying_parrot_sprite: AnimatedSprite,
-    body: PhysicsBody,
+    reference_pos: Vec2,
+    current_x: f32,
+    speed_x: f32,
+    facing: bool,
     lived: f32,
     countdown: f32,
 }
@@ -45,41 +50,27 @@ impl FlyingParrot {
         );
 
         let facing_x_factor = if parrot_body.facing { 1. } else { -1. };
-        let mut resources = storage::get_mut::<Resources>();
 
-        let speed = vec2(
-            facing_x_factor * FLYING_PARROT_INITIAL_SPEED_X_REL,
-            FLYING_PARROT_INITIAL_SPEED_Y,
-        );
-
-        let flying_parrot_mount_pos = vec2(
-            parrot_body.pos.x - facing_x_factor * FLYING_PARROT_MOUNT_X_REL,
-            parrot_body.pos.y,
-        );
-
-        let actor = resources.collision_world.add_actor(
-            flying_parrot_mount_pos,
-            FLYING_PARROT_WIDTH as i32,
-            FLYING_PARROT_HEIGHT as i32,
-        );
-
-        let body = PhysicsBody {
-            pos: flying_parrot_mount_pos,
-            facing: parrot_body.facing,
-            angle: 0.0,
-            speed,
-            collider: Some(actor),
-            on_ground: false,
-            last_frame_on_ground: false,
-            have_gravity: true,
-        };
+        let speed_x = facing_x_factor * FLYING_PARROT_SPEED_X;
+        let current_x = parrot_body.pos.x - facing_x_factor * FLYING_PARROT_MOUNT_X_REL;
 
         Self {
             flying_parrot_sprite,
-            body,
+            current_x,
+            reference_pos: vec2(parrot_body.pos.x, parrot_body.pos.y),
+            speed_x,
+            facing: parrot_body.facing,
             lived: 0.0,
             countdown: FLYING_PARROT_COUNTDOWN_DURATION,
         }
+    }
+
+    fn current_y(&self) -> f32 {
+        // Start always from the negative value, so that the motion always starts upwards.
+        let diff_x = -(self.current_x - self.reference_pos.x).abs();
+        let displacement =
+            (diff_x / FLYING_PARROT_Y_FREQ_SLOWDOWN).sin() * (FLYING_PARROT_MAX_AMPLITUDE / 2.);
+        self.reference_pos.y + displacement
     }
 }
 
@@ -102,19 +93,21 @@ impl FlyingParrots {
 impl scene::Node for FlyingParrots {
     fn fixed_update(mut node: RefMut<Self>) {
         for flying_parrot in &mut node.flying_parrots {
-            flying_parrot.body.update();
             flying_parrot.lived += get_frame_time();
+            flying_parrot.current_x += flying_parrot.speed_x * get_frame_time();
         }
 
         node.flying_parrots.retain(|flying_parrot| {
             let hit_fxses = &mut storage::get_mut::<Resources>().hit_fxses;
-            let explosion_position =
-                flying_parrot.body.pos + vec2(FLYING_PARROT_WIDTH / 2., FLYING_PARROT_HEIGHT / 2.);
+            let explosion_position = vec2(
+                flying_parrot.current_x + FLYING_PARROT_WIDTH / 2.,
+                flying_parrot.current_y() + FLYING_PARROT_HEIGHT / 2.,
+            );
 
             if flying_parrot.lived < flying_parrot.countdown {
                 let flying_parrot_hitbox = Rect::new(
-                    flying_parrot.body.pos.x,
-                    flying_parrot.body.pos.y,
+                    flying_parrot.current_x,
+                    flying_parrot.current_y(),
                     FLYING_PARROT_WIDTH,
                     FLYING_PARROT_HEIGHT,
                 );
@@ -133,7 +126,7 @@ impl scene::Node for FlyingParrots {
                             .unwrap()
                             .shake();
 
-                        let direction = flying_parrot.body.pos.x
+                        let direction = flying_parrot.current_x
                             > (player.body.pos.x + PLAYER_HITBOX_WIDTH / 2.);
                         player.kill(direction);
 
@@ -157,13 +150,13 @@ impl scene::Node for FlyingParrots {
 
             draw_texture_ex(
                 resources.flying_parrots,
-                flying_parrot.body.pos.x,
-                flying_parrot.body.pos.y,
+                flying_parrot.current_x,
+                flying_parrot.current_y(),
                 color::WHITE,
                 DrawTextureParams {
                     source: Some(flying_parrot.flying_parrot_sprite.frame().source_rect),
                     dest_size: Some(flying_parrot.flying_parrot_sprite.frame().dest_size),
-                    flip_x: flying_parrot.body.facing,
+                    flip_x: flying_parrot.facing,
                     rotation: 0.0,
                     ..Default::default()
                 },

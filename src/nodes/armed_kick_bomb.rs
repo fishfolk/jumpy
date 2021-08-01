baf80a6ed9dbd11f174db84bb19267fb2df4ab11
Ole A. Sjo Fasting <oasf@polygo.no>
2021-08-01 18:30:44 +0200

    kick bombs are single use and can only be kicked if moving towards it

--- src/nodes/armed_kick_bomb.rs
+++ src/nodes/armed_kick_bomb.rs
@@ -33,6 +33,7 @@ pub struct ArmedKickBomb {
 impl ArmedKickBomb {
     pub const COLLIDER_WIDTH: f32 = 30.0;
     pub const COLLIDER_HEIGHT: f32 = 30.0;
+    pub const KICK_SPEED_THRESHOLD: f32 = 150.0;
     pub const KICK_FORCE: f32 = 900.0;
     pub const COUNTDOWN_DURATION: f32 = 3.0;
     pub const EXPLOSION_RADIUS: f32 = 150.0;
@@ -105,6 +106,7 @@ impl Node for ArmedKickBomb {
         node.lived += get_frame_time();
 
         let hitbox = Rect::new(node.body.pos.x, node.body.pos.y, ArmedKickBomb::COLLIDER_WIDTH, ArmedKickBomb::COLLIDER_HEIGHT);
+
         for mut player in scene::find_nodes_by_type::<Player>() {
             if hitbox.overlaps(&player.get_hitbox()) {
                 if let Some((weapon, _, _, gun)) = player.weapon.as_mut() {
@@ -115,22 +117,18 @@ impl Node for ArmedKickBomb {
         }
 
         if node.lived < ArmedKickBomb::COUNTDOWN_DURATION {
-            let hit_box = Rect::new(
-                node.body.pos.x,
-                node.body.pos.y,
-                30.0,
-                30.0,
-            );
             for player in scene::find_nodes_by_type::<crate::nodes::Player>() {
-                let is_overlapping =
-                    hit_box.overlaps(&player.get_hitbox());
-                if is_overlapping && hit_box.y + 36.0 >= player.body.pos.y + Player::LEGS_THRESHOLD {
-                    let direction = node.body.pos.x > (player.body.pos.x + 10.);
-                    if direction == player.body.facing {
-                        node.body.speed.x = if direction {
-                            Self::KICK_FORCE
-                        } else {
-                            -Self::KICK_FORCE
+                if player.body.speed.x >= ArmedKickBomb::KICK_SPEED_THRESHOLD || player.body.speed.x <= -ArmedKickBomb::KICK_SPEED_THRESHOLD {
+                    let is_overlapping =
+                        hitbox.overlaps(&player.get_hitbox());
+                    if is_overlapping && hitbox.y + 36.0 >= player.body.pos.y + Player::LEGS_THRESHOLD {
+                        let direction = node.body.pos.x > (player.body.pos.x + 10.);
+                        if direction == player.body.facing {
+                            node.body.speed.x = if direction {
+                                Self::KICK_FORCE
+                            } else {
+                                -Self::KICK_FORCE
+                            }
                         }
                     }
                 }

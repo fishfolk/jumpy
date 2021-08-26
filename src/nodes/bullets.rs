ab9b490a81ef5646f1310e0f6a32f198b8205dec
grufkork <griffin.hiscoke@me.com>
2021-08-26 19:00:21 +0200

    Nicer bullets/animated particles
    I added the bullet sprite from the Gun texture, and experimented with adding smoke puff trails after bullets. This doesn't use the regular particles, because those don't support particles. Implementation could be cleaner, but I've heard it's more about establishing a style than writing extensible code.
    
    Stylistically, I think it doesn't feel intuitive yet actually works pretty well. Though it's difficult to look at it with fresh eyes, so if anyone else would like to take a look that would be great. For the MG, the smoke sometimes crowds out the bullets which lack a lot of contrast. Also, someone more artsy might want to remake thesmoke...

--- src/nodes/bullets.rs
+++ src/nodes/bullets.rs
@@ -1,5 +1,10 @@
 use macroquad::{
-    experimental::{collections::storage, scene::RefMut},
+    color,
+    experimental::{
+        animation::{AnimatedSprite, Animation},
+        collections::storage,
+        scene::RefMut,
+    },
     prelude::*,
 };
 
@@ -12,19 +17,54 @@ struct Bullet {
     size: f32,
     lived: f32,
     lifetime: f32,
+    rot: f32,
+    smoke_cooldown: f32,
+}
+
+struct Smoke {
+    pos: Vec2,
+    age: f32,
+    rot: f32,
 }
 
 pub struct Bullets {
     bullets: Vec<Bullet>,
+    sprite: AnimatedSprite,
+    smoke: AnimatedSprite,
+    smokes: Vec<Smoke>,
 }
 
 impl Bullets {
     pub const BULLET_SPEED: f32 = 500.0;
     pub const BULLET_LIFETIME: f32 = 0.9;
+    pub const SMOKE_SPAWN_RATE: f32 = 8.0;
 
     pub fn new() -> Bullets {
         Bullets {
             bullets: Vec::with_capacity(200),
+            smokes: Vec::with_capacity(200),
+            sprite: AnimatedSprite::new(
+                92,
+                32,
+                &[Animation {
+                    name: "fly".to_string(),
+                    row: 3,
+                    frames: 1,
+                    fps: 1,
+                }],
+                false,
+            ),
+            smoke: AnimatedSprite::new(
+                32,
+                32,
+                &[Animation {
+                    name: "anim".to_string(),
+                    row: 0,
+                    frames: 10,
+                    fps: 15,
+                }],
+                false,
+            ),
         }
     }
 
@@ -37,20 +77,77 @@ impl Bullets {
             size,
             lived: 0.0,
             lifetime: Self::BULLET_LIFETIME,
+            rot: 0.0,
+            smoke_cooldown: rand::gen_range(3.0, 13.0),
         });
     }
 }
 
 impl scene::Node for Bullets {
-    fn draw(node: RefMut<Self>) {
-        for bullet in &node.bullets {
-            draw_circle(
+    fn draw(mut node: RefMut<Self>) {
+        let sprite = node.sprite.clone(); // Ugly copy, but things are getting reworked anyways
+        let resources = storage::get_mut::<Resources>();
+        for i in 0..node.bullets.len() {
+            {
+                let bullet = &mut node.bullets[i];
+
+                bullet.rot += 0.2;
+
+                draw_texture_ex(
+                    resources.gun,
+                    bullet.pos.x - 52.0 * bullet.size * 0.25,
+                    bullet.pos.y - 14.0 * bullet.size * 0.25,
+                    color::WHITE,
+                    DrawTextureParams {
+                        source: Some(sprite.frame().source_rect),
+                        dest_size: Some(sprite.frame().dest_size * bullet.size * 0.25),
+                        rotation: bullet.rot,
+                        pivot: Some(vec2(bullet.pos.x, bullet.pos.y)),
+                        ..Default::default()
+                    },
+                );
+
+                bullet.smoke_cooldown -= 1.;
+            }
+            if node.bullets[i].smoke_cooldown <= 0. {
+                node.bullets[i].smoke_cooldown = Self::SMOKE_SPAWN_RATE;
+                let pos = node.bullets[i].pos;
+                node.smokes.push(Smoke {
+                    pos,
+                    age: 0.0,
+                    rot: rand::gen_range(0.0, 360.0),
+                })
+            }
+
+            /*draw_circle(
                 bullet.pos.x,
                 bullet.pos.y,
                 bullet.size,
                 Color::new(1.0, 1.0, 0.8, 1.0),
-            );
+            );*/
         }
+
+        let mut sm = node.smoke.clone();
+        for s in &mut node.smokes {
+            sm.set_frame(s.age as u32 % 10);
+            sm.update();
+            draw_texture_ex(
+                resources.smoke_trail,
+                s.pos.x - 32.0,
+                s.pos.y - 32.0,
+                color::WHITE,
+                DrawTextureParams {
+                    source: Some(sm.frame().source_rect),
+                    dest_size: Some(sm.frame().dest_size * 2.0),
+                    rotation: s.rot,
+                    //pivot: Some(vec2(bullet.pos.x, bullet.pos.y)),
+                    ..Default::default()
+                },
+            );
+            s.age += 1.0;
+        }
+
+        node.smokes.retain(|s| s.age < 10.0);
     }
 
     fn fixed_update(mut node: RefMut<Self>) {

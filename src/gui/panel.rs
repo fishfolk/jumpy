f45ddd83cc761c89e444760eef1b7a69de3faa5f
Ole A. Sjo Fasting <oasf@polygo.no>
2021-11-16 20:20:39 +0100

    panels with background colors in normal draw order

--- src/gui/panel.rs
+++ src/gui/panel.rs
@@ -14,24 +14,38 @@ use super::{GuiResources, WINDOW_MARGIN_H, WINDOW_MARGIN_V};
 pub struct Panel {
     id: Id,
     title: Option<String>,
+    is_title_centered: bool,
     size: Vec2,
     position: Vec2,
+    background_color: Option<Color>,
 }
 
 impl Panel {
+    const BG_OFFSET: f32 = 12.0;
+
     pub fn new(id: Id, size: Vec2, position: Vec2) -> Self {
         Panel {
             id,
             title: None,
+            is_title_centered: false,
             size,
             position,
+            background_color: None,
         }
     }
 
     #[allow(dead_code)]
-    pub fn with_title(self, title: &str) -> Self {
+    pub fn with_title(self, title: &str, is_centered: bool) -> Self {
         Panel {
             title: Some(title.to_string()),
+            is_title_centered: is_centered,
+            ..self
+        }
+    }
+
+    pub fn with_background_color(self, color: Color) -> Self {
+        Panel {
+            background_color: Some(color),
             ..self
         }
     }
@@ -42,7 +56,20 @@ impl Panel {
     pub fn ui<F: FnOnce(&mut Ui, Vec2)>(&self, ui: &mut Ui, f: F) {
         {
             let gui_resources = storage::get::<GuiResources>();
-            ui.push_skin(&gui_resources.skins.panel_group);
+
+            if let Some(background_color) = self.background_color {
+                ui.push_skin(&gui_resources.skins.panel_no_bg);
+
+                draw_rectangle(
+                    self.position.x + Self::BG_OFFSET,
+                    self.position.y + Self::BG_OFFSET,
+                    self.size.x - (Self::BG_OFFSET * 2.0),
+                    self.size.y - (Self::BG_OFFSET * 2.0),
+                    background_color,
+                );
+            } else {
+                ui.push_skin(&gui_resources.skins.panel);
+            }
         }
 
         let _ = widgets::Button::new("")
@@ -59,9 +86,14 @@ impl Panel {
             let gui_resources = storage::get::<GuiResources>();
             ui.push_skin(&gui_resources.skins.window_header);
 
-            ui.label(content_position, title);
-
             let label_size = ui.calc_size(title);
+            let mut label_position = content_position;
+
+            if self.is_title_centered {
+                label_position.x += ((self.size.x - label_size.x) / 2.0) - WINDOW_MARGIN_H;
+            }
+
+            ui.label(label_position, title);
 
             content_size.y -= label_size.y;
             content_position.y += label_size.y;

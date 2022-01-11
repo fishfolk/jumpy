066749bd59e1d474f570fe9d77d399c2f32a82e0
Ole A. Sjo Fasting <oasf@polygo.no>
2022-01-11 14:23:19 +0100

    format

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,3 +1,6 @@
+//! This contains some networking prototyping that was done before the refactoring to ECS
+//! Except for the mock API, there is probably not much that can be used here, now....
+
 mod api;
 
 use macroquad::prelude::*;
@@ -7,9 +10,6 @@ use serde::{Deserialize, Serialize};
 pub use api::{Account, AccountId, Api, Lobby, LobbyId};
 
 use crate::json;
-
-//! This contains some networking prototyping that was done before the refactoring to ECS
-//! Except for the mock API, there is probably not much that can be used here, now....
 
 pub const UDP_CHUNK_SIZE: usize = 512;
 

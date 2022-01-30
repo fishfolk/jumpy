83241aa0a9218e788411d417260e7d3bf63d91f4
Ole A. Sjo Fasting <oasf@polygo.no>
2022-01-30 10:07:36 +0100

    fixes jump not being interrupted when attacking

--- src/player/state.rs
+++ src/player/state.rs
@@ -135,6 +135,12 @@ pub fn update_player_states(world: &mut World) {
         // Integration
         if state.is_dead || state.is_attacking || state.is_incapacitated || state.is_sliding {
             body.has_friction = true;
+
+            if state.is_attacking {
+                state.is_jumping = false;
+                state.jump_frame_counter = 0;
+                body.has_mass = true;
+            }
         } else {
             body.has_friction = false;
 

42699330999db7d843655a052661054c5ce28eb4
Zicklag <zicklag@katharostech.com>
2022-09-27 19:44:06 -0500

    Fix Animated Menu Background on WASM

--- assets/ui/menu-background-zoom.ts
+++ assets/ui/menu-background-zoom.ts
@@ -3,6 +3,15 @@ const MenuBackground: BevyType<MenuBackground> = {
   typeName: "jumpy::ui::main_menu::MainMenuBackground",
 };
 
+// This is a workaround for the fact that the Bevy types aren't getting set on WASM for some
+// reason. Not a long-term solution, needs investigation.
+const Time: BevyType<Time> = {
+  typeName: "bevy_time::time::Time",
+};
+const Transform: BevyType<Transform> = {
+  typeName: "bevy_transform::components::transform::Transform",
+};
+
 export default {
   update() {
     const time = world.resource(Time);
@@ -14,7 +23,7 @@ export default {
       transform.scale.x = scale;
       transform.scale.y = scale;
 
-      let offset = Math.sin(time.seconds_since_startup * 0.4  ) * 50;
+      let offset = Math.sin(time.seconds_since_startup * 0.4) * 50;
       transform.translation.x = offset + 60;
       transform.translation.y = offset * 0.2 - 20;
     }

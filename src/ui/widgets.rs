134e9d3ed1e6acaaaf113a3b9911ea03f577e035
Zicklag <zicklag@katharostech.com>
2022-09-30 16:44:14 -0500

    More UI Reorganization

--- src/ui/widgets.rs
+++ src/ui/widgets.rs
@@ -6,6 +6,7 @@ pub mod bordered_button;
 pub mod bordered_frame;
 pub mod progress_bar;
 
+/// Extensions on [`egui::Ui`] for custom widgets
 pub trait EguiUiExt {
     fn themed_label(self, font_meta: &FontMeta, label: &str) -> egui::Response;
 }
@@ -17,5 +18,35 @@ impl EguiUiExt for &mut egui::Ui {
                 .color(font_meta.color)
                 .font(font_meta.font_id()),
         ))
+    }
+}
+
+/// Extension trait with helpers for the egui context
+pub trait EguiContextExt {
+    /// Clear the UI focus
+    fn clear_focus(self);
+}
+
+impl EguiContextExt for &egui::Context {
+    fn clear_focus(self) {
+        self.memory().request_focus(egui::Id::null());
+    }
+}
+
+/// Extension trait with helpers for egui responses
+pub trait EguiResponseExt {
+    /// Set this response to focused if nothing else is focused
+    fn focus_by_default(self, ui: &mut egui::Ui) -> egui::Response;
+}
+
+impl EguiResponseExt for egui::Response {
+    fn focus_by_default(self, ui: &mut egui::Ui) -> egui::Response {
+        if ui.ctx().memory().focus().is_none() {
+            ui.ctx().memory().request_focus(self.id);
+
+            self
+        } else {
+            self
+        }
     }
 }

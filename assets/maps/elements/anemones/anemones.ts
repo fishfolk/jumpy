82570386504548e11586c42ced0972bb48af6baf
Zicklag <zicklag@katharostech.com>
2022-10-05 21:09:11 -0500

    Add Anemonies Map Element

--- assets/maps/elements/anemones/anemones.ts
+++ assets/maps/elements/anemones/anemones.ts
@@ -1,1 +1,29 @@
-export default {};
+type AnimatedSprite = {
+  start: usize;
+  end: usize;
+  atlas_path: string;
+  flip_x: boolean;
+  flip_y: boolean;
+  repeat: boolean;
+  fps: f32;
+};
+const AnimatedSprite: BevyType<AnimatedSprite> = {
+  typeName: "jumpy::animation::AnimatedSprite",
+};
+
+export default {
+  preUpdate() {
+    for (const entity of MapElement.getSpawnedEntities()) {
+      world.insert(
+        entity,
+        Value.create(AnimatedSprite, {
+          start: 5,
+          end: 9,
+          repeat: true,
+          fps: 6,
+          atlas_path: Assets.absolutePath("../../resources/default_decoration.atlas.yaml"),
+        })
+      );
+    }
+  },
+};

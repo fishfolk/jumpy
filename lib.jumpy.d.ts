0a565f24f1131c872b165d6d4f405f56d6a626ca
Zicklag <zicklag@katharostech.com>
2022-10-08 19:17:10 -0500

    Add a Script State and Info Helper

--- lib.jumpy.d.ts
+++ lib.jumpy.d.ts
@@ -1,7 +1,22 @@
 declare namespace MapElement {
-  function getSpawnedEntities(): Entity[]
+  function getSpawnedEntities(): Entity[];
+  function mapReset(): boolean;
 }
 
 declare namespace Assets {
   function getHandleId(relative_path: string): HandleId;
+}
+
+// All handles have the same type, so just alias here
+type HandleJsScript = HandleImage;
+
+declare interface ScriptInfo {
+  path: string;
+  handle: HandleJsScript;
+  handle_id_hash: string;
+}
+
+declare namespace ScriptInfo {
+  function get(): ScriptInfo;
+  function state<T>(init?: T): T;
 }
--- src/scripting/ops.rs
+++ src/scripting/ops.rs
@@ -2,6 +2,7 @@ use bevy_mod_js_scripting::OpMap;
 
 mod asset;
 mod map;
+mod script_info;
 
 pub fn get_ops() -> OpMap {
     let mut ops = OpMap::default();
@@ -13,6 +14,10 @@ pub fn get_ops() -> OpMap {
     ops.insert(
         "jumpy_asset_get_handle_id",
         Box::new(asset::AssetGetHandleId),
+    );
+    ops.insert(
+        "jumpy_script_info_get",
+        Box::new(script_info::ScriptInfoGet),
     );
 
     ops

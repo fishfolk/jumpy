b3d15ae7f055588e65203378a7f62aeb8066096a
Zicklag <zicklag@katharostech.com>
2022-11-03 18:18:00 -0500

    Clean up dead.ts State

--- assets/player/states/dead.ts
+++ assets/player/states/dead.ts
@@ -1,39 +1,33 @@
 const scriptId = Script.getInfo().path;
 
-const initState = {
-  frames: 0,
-};
-
 /** Responsible for transitioning players to the dead state whenever they are killed */
 export default {
   playerStateTransition() {
     const players = world.query(PlayerState);
 
+    // Transition all players tht have been killed to this state
     for (const event of Player.killEvents()) {
       const [playerState] = players.get(event.player);
       playerState.id = Assets.getAbsolutePath("./dead.ts");
     }
   },
   handlePlayerState() {
-    const players = world.query(PlayerState, AnimatedSprite, AnimationBankSprite);
+    const players = world.query(PlayerState, AnimationBankSprite);
 
     for (const {
       entity,
-      components: [playerState, animatedSprite, animationBankSprite],
+      components: [playerState, animationBankSprite],
     } of players) {
       // In this state
       if (playerState.id != scriptId) continue;
 
-      const state = Script.getEntityState(entity, initState);
-
-      if (state.frames == 0) {
-        animatedSprite.index = 0;
+      // Set animation when we enter the state
+      if (playerState.age == 0) {
         animationBankSprite.current_animation = "death_1";
       }
 
-      state.frames += 1;
-
-      if (state.frames >= 90) {
+      // Despawn player after 1.5 seconds ( 90 frames )
+      if (playerState.age >= 90) {
         Player.despawn(entity);
         Script.setEntityState(entity, undefined);
       }

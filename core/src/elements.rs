6f86b14e0daf1d4592f7c2ccb1f20a4a7208057e
Tekhnae Raav <tekhnaeraav@katharostech.com>
2023-03-02 13:38:45 -0600

    Add small snail critters

--- core/src/elements.rs
+++ core/src/elements.rs
@@ -9,6 +9,7 @@ pub mod mine;
 pub mod musket;
 pub mod player_spawner;
 pub mod slippery_seaweed;
+pub mod snail;
 pub mod sproinger;
 pub mod stomp_boots;
 pub mod sword;
@@ -43,6 +44,7 @@ pub fn install(session: &mut GameSession) {
     sword::install(session);
     grenade::install(session);
     crab::install(session);
+    snail::install(session);
     kick_bomb::install(session);
     mine::install(session);
     musket::install(session);

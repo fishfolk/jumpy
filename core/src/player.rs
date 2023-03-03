649d1a9c90d2a8a169323108f85a961d7878e902
Zicklag <zicklag@katharostech.com>
2023-03-03 12:57:33 -0600

    feat: give AI players a sword and make them attack player 1.

--- core/src/player.rs
+++ core/src/player.rs
@@ -16,6 +16,7 @@ pub fn install(session: &mut GameSession) {
         .add_system_to_stage(CoreStage::First, player_ai_system)
         .add_system_to_stage(CoreStage::PostUpdate, play_itemless_fin_animations)
         .add_system_to_stage(CoreStage::PostUpdate, player_facial_animations)
+        .add_system_to_stage(CoreStage::Last, delete_dead_ai_swords)
         .add_system_to_stage(CoreStage::Last, update_player_layers);
 }
 
@@ -215,27 +216,37 @@ pub struct AiPlayer {
     movement_buffer: Option<VecDeque<PlayerControl>>,
 }
 
-#[derive(Debug, TypeUlid, Clone, Copy)]
+#[derive(Debug, TypeUlid, Clone)]
 #[ulid = "01GRA68NKYG6X7C5D0WNA5W1VX"]
-pub struct PathfindingDebugLine {
-    pub entity: Entity,
+pub struct PathfindingDebugLines {
+    pub entities: Vec<Entity>,
 }
 
-impl FromWorld for PathfindingDebugLine {
+impl FromWorld for PathfindingDebugLines {
     fn from_world(world: &mut World) -> Self {
-        let entity = world
+        let entities = world
             .run_initialized_system(
                 |mut entities: ResMut<Entities>, mut transforms: CompMut<Transform>| {
-                    let ent = entities.create();
+                    let ents = (0..MAX_PLAYERS)
+                        .into_iter()
+                        .map(|_| {
+                            let ent = entities.create();
 
-                    transforms.insert(ent, Transform::from_translation(Vec3::new(0.0, 0.0, -1.0)));
+                            transforms.insert(
+                                ent,
+                                Transform::from_translation(Vec3::new(0.0, 0.0, -1.0)),
+                            );
 
-                    Ok(ent)
+                            ent
+                        })
+                        .collect::<Vec<_>>();
+
+                    Ok(ents)
                 },
             )
             .unwrap();
 
-        Self { entity }
+        Self { entities }
     }
 }
 
@@ -247,11 +258,13 @@ fn player_ai_system(
     player_indexes: Comp<PlayerIdx>,
     map: Res<LoadedMap>,
     transforms: Comp<Transform>,
-    pathfinding_debug_line: ResMut<PathfindingDebugLine>,
+    pathfinding_debug_line: ResMut<PathfindingDebugLines>,
     mut paths: CompMut<Path2d>,
     bodies: Comp<KinematicBody>,
     debug_settings: Res<DebugSettings>,
 ) {
+    const SWORD_SWING_DIST: f32 = 50.0;
+
     let Some(player1) = entities
         .iter_with(&player_indexes)
         .filter(|(_ent, idx)| idx.0 == 0)
@@ -260,25 +273,25 @@ fn player_ai_system(
             return
         };
     let transform = transforms.get(player1).unwrap();
-    let pos = transform.translation.truncate();
-    let tile = (pos / map.tile_size).floor().as_ivec2();
+    let player1_pos = transform.translation.truncate();
+    let tile = (player1_pos / map.tile_size).floor().as_ivec2();
     let target_node = NavNode(tile);
 
     for (ai_ent, (player_idx, transform, ai_player)) in
         entities.iter_with((&player_indexes, &transforms, &mut ai_players))
     {
-        let pos = transform.translation.truncate();
-        let tile = (pos / map.tile_size).floor().as_ivec2();
+        let ai_pos = transform.translation.truncate();
+        let tile = (ai_pos / map.tile_size).floor().as_ivec2();
         let current_node = NavNode(tile);
 
         // Complete any previous movement instructions if we are in the middle of any
         if let Some(movement_buffer) = &mut ai_player.movement_buffer {
             if let Some(control) = movement_buffer.pop_front() {
-                player_inputs.players[1].control = control;
+                player_inputs.players[player_idx.0].control = control;
                 if movement_buffer.is_empty() {
                     ai_player.movement_buffer = None;
                 }
-                return;
+                continue;
             }
         }
 
@@ -293,14 +306,19 @@ fn player_ai_system(
         if let Some((_cost, path)) = path {
             if debug_settings.show_pathfinding_lines {
                 paths.insert(
-                    pathfinding_debug_line.entity,
+                    pathfinding_debug_line.entities[player_idx.0],
                     Path2d {
                         points: path
                             .iter()
                             .map(|x| x.0.as_vec2() * map.tile_size + map.tile_size / 2.0)
                             .collect(),
                         thickness: 2.0,
-                        color: [1.0, 0.0, 0.0, 1.0],
+                        color: [
+                            [1.0, 0.0, 0.0, 1.0],
+                            [0.0, 1.0, 0.0, 1.0],
+                            [0.0, 0.0, 1.0, 1.0],
+                            [1.0, 0.0, 1.0, 1.0],
+                        ][player_idx.0],
                         ..default()
                     },
                 );
@@ -329,11 +347,18 @@ fn player_ai_system(
                     ai_player.movement_buffer = Some(movement_buffer)
                 }
             }
+
+            if (player1_pos - ai_pos).length() < SWORD_SWING_DIST {
+                player_inputs.players[player_idx.0]
+                    .control
+                    .shoot_just_pressed = true;
+                player_inputs.players[player_idx.0].control.shoot_pressed = true;
+            }
         } else if debug_settings.show_pathfinding_lines {
             let pos =
                 current_node.0.as_vec2() * map.tile_size + map.tile_size / 2.0 - vec2(0.0, 4.0);
             paths.insert(
-                pathfinding_debug_line.entity,
+                pathfinding_debug_line.entities[player_idx.0],
                 Path2d {
                     points: vec![pos, pos + vec2(0.0, 4.0)],
                     thickness: 8.0,
@@ -344,12 +369,13 @@ fn player_ai_system(
         }
 
         if !debug_settings.show_pathfinding_lines {
-            paths.remove(pathfinding_debug_line.entity);
+            paths.remove(pathfinding_debug_line.entities[player_idx.0]);
         }
     }
 }
 
 fn hydrate_players(
+    mut commands: Commands,
     mut entities: ResMut<Entities>,
     player_inputs: Res<PlayerInputs>,
     player_indexes: Comp<PlayerIdx>,
@@ -394,9 +420,6 @@ fn hydrate_players(
         emote_states.insert(player_entity, default());
         animation_bank_sprites.insert(player_entity, animation_bank_sprite);
         inventories.insert(player_entity, default());
-        if is_ai {
-            ai_players.insert(player_entity, default());
-        }
 
         atlas_sprites.insert(
             player_entity,
@@ -483,6 +506,101 @@ fn hydrate_players(
                 sync_animation: false,
             },
         );
+
+        // Handle AI players
+        if is_ai {
+            ai_players.insert(player_entity, default());
+
+            // Give the player a sword NOTE: It's not good that we're duplicating the sword hydrate
+            // functionality here, and this is pretty hacky, but the AI as it stands is temporary
+            // anyway, so it's fine for now.
+            commands.add(
+                move |mut entities: ResMut<Entities>,
+                      mut swords: CompMut<sword::Sword>,
+                      mut element_handles: CompMut<ElementHandle>,
+                      element_assets: BevyAssets<ElementMeta>,
+                      mut hydrated: CompMut<MapElementHydrated>,
+                      mut bodies: CompMut<KinematicBody>,
+                      mut atlas_sprites: CompMut<AtlasSprite>,
+                      mut items: CompMut<Item>,
+                      mut transforms: CompMut<Transform>,
+                      game_meta: Res<CoreMetaArc>,
+                      mut attachments: CompMut<PlayerBodyAttachment>,
+                      mut inventories: CompMut<Inventory>| {
+                    let element_handle = element_handles
+                        .iter()
+                        .find(|handle| {
+                            matches!(
+                                element_assets
+                                    .get(&handle.get_bevy_handle())
+                                    .unwrap()
+                                    .builtin,
+                                BuiltinElementKind::Sword { .. }
+                            )
+                        })
+                        .unwrap();
+                    let element_meta = element_assets
+                        .get(&element_handle.get_bevy_handle())
+                        .unwrap();
+                    if let BuiltinElementKind::Sword {
+                        atlas,
+                        body_size,
+                        can_rotate,
+                        bounciness,
+                        grab_offset,
+                        ..
+                    } = &element_meta.builtin
+                    {
+                        let sword_ent = entities.create();
+                        inventories.insert(player_entity, Inventory(Some(sword_ent)));
+                        items.insert(sword_ent, Item);
+                        swords.insert(sword_ent, sword::Sword::default());
+                        atlas_sprites.insert(sword_ent, AtlasSprite::new(atlas.clone()));
+                        transforms.insert(sword_ent, default());
+                        element_handles.insert(sword_ent, element_handle.clone());
+                        hydrated.insert(sword_ent, MapElementHydrated);
+                        bodies.insert(
+                            sword_ent,
+                            KinematicBody {
+                                shape: ColliderShape::Rectangle { size: *body_size },
+                                has_mass: true,
+                                has_friction: true,
+                                can_rotate: *can_rotate,
+                                bounciness: *bounciness,
+                                gravity: game_meta.physics.gravity,
+                                ..default()
+                            },
+                        );
+                        attachments.insert(
+                            sword_ent,
+                            PlayerBodyAttachment {
+                                player: player_entity,
+                                offset: grab_offset.extend(1.0),
+                                sync_animation: false,
+                            },
+                        );
+                    }
+                },
+            );
+        }
+    }
+}
+
+/// System that makes sure the swords held by AI are despawned when they are killed.
+fn delete_dead_ai_swords(
+    mut entities: ResMut<Entities>,
+    ai_players: Comp<AiPlayer>,
+    items: Comp<Item>,
+    dropped: Comp<ItemDropped>,
+) {
+    let mut to_kill = Vec::new();
+    for (ent, (_item, dropped)) in entities.iter_with((&items, &dropped)) {
+        if ai_players.contains(dropped.player) {
+            to_kill.push(ent);
+        }
+    }
+    for entity in to_kill {
+        entities.kill(entity);
     }
 }
 

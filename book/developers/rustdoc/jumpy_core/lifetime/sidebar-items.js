9772de796a4d123bf45f821722e12b569fbae1be
zicklag <zicklag@katharostech.com>
2023-03-09 20:15:55 +0000

    Deploying to gh-pages from @ fishfolk/jumpy@400de0ce1b39e2a34fd5e55e006b5a0caff130ff 🚀

--- book/developers/rustdoc/jumpy_core/lifetime/sidebar-items.js
+++ book/developers/rustdoc/jumpy_core/lifetime/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":[["install",""]],"struct":[["Lifetime","The lifetime state of an entity"]]};
+window.SIDEBAR_ITEMS = {"fn":[["install",""]],"struct":[["Invincibility","A timer that can be used to make an entity invincible for a certain amount of time"],["Lifetime","The lifetime state of an entity"]]};

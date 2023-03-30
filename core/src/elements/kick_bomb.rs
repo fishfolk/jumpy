19987e3efb5b6c08069e8f50481188cce9c2dab9
Christopher Navarro <chrisidxloop@gmail.com>
2023-03-30 16:09:29 -0400

    simplified intialization of IdleKickBomb.arm_delay, IdleKickBomb.fuse_time

--- core/src/elements/kick_bomb.rs
+++ core/src/elements/kick_bomb.rs
@@ -148,11 +148,11 @@ fn update_idle_kick_bombs(
                         entity,
                         LitKickBomb {
                             arm_delay: Timer::new(
-                                Duration::from_secs_f32(arm_delay.as_secs_f32()),
+                                arm_delay,
                                 TimerMode::Once,
                             ),
                             fuse_time: Timer::new(
-                                Duration::from_secs_f32(fuse_time.as_secs_f32()),
+                                fuse_time,
                                 TimerMode::Once,
                             ),
                         },

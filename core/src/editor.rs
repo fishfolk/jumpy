6671fdaf76fbdde0517ed55a6c5e347f2ac1f8b7
austin <galaky2005@gmail.com>
2023-04-28 23:54:27 -0400

    refactored, but need to move impl_system_param macro to central location; need to test;

--- core/src/editor.rs
+++ core/src/editor.rs
@@ -6,17 +6,203 @@ pub fn install(session: &mut CoreSession) {
         .add_system_to_stage(CoreStage::PreUpdate, handle_editor_input);
 }
 
+// TODO move these structs to their own file
+//impl_system_param! {
+    struct MapInterface<'a> {
+        commands: Commands<'a>,
+        entities: ResMut<'a, Entities>,
+        spawned_map_meta: ResMut<'a, SpawnedMapMeta>,
+        element_handles: CompMut<'a, ElementHandle>,
+        transforms: CompMut<'a, Transform>,
+        spawned_map_layer_metas: CompMut<'a, SpawnedMapLayerMeta>,
+        tile_layers: CompMut<'a, TileLayer>,
+        tiles: CompMut<'a, Tile>,
+        tile_collisions: CompMut<'a, TileCollisionKind>
+    }
+//}
+
+impl<'a> MapInterface<'a> {
+    fn create_element(&mut self, element_meta_handle: &Handle<ElementMeta>, translation: &Vec2, layer_index: usize) {
+        let entity = self.entities.create();
+        self.element_handles.insert(entity, ElementHandle(element_meta_handle.clone()));
+        let z_depth = z_depth_for_map_layer(layer_index);
+        self.transforms.insert(
+            entity,
+            Transform::from_translation(translation.extend(z_depth)),
+        );
+        self.spawned_map_layer_metas.insert(
+            entity,
+            SpawnedMapLayerMeta {
+                layer_idx: layer_index,
+            },
+        );
+    }
+    fn create_layer(&mut self, name: String) {
+        let entity = self.entities.create();
+        let layer_index = self.spawned_map_meta.layer_names.len();
+        self.spawned_map_meta.layer_names = self.spawned_map_meta
+            .layer_names
+            .clone()
+            .iter()
+            .cloned()
+            .chain([name])
+            .collect();
+        self.spawned_map_layer_metas.insert(entity, SpawnedMapLayerMeta { layer_idx: layer_index });
+        self.tile_layers.insert(
+            entity,
+            TileLayer::new(
+                self.spawned_map_meta.grid_size,
+                self.spawned_map_meta.tile_size,
+                default(),
+            ),
+        );
+        self.transforms.insert(
+            entity,
+            Transform::from_translation(Vec3::new(
+                0.0,
+                0.0,
+                z_depth_for_map_layer(layer_index),
+            )),
+        );
+    }
+    fn delete_layer(&mut self, layer_index: usize) {
+        let layer_count = self.spawned_map_meta.layer_names.len();
+        let layers_to_decrement = layer_count - layer_index;
+        self.spawned_map_meta.layer_names = self.spawned_map_meta
+            .layer_names
+            .clone()
+            .iter()
+            .cloned()
+            .enumerate()
+            .filter_map(|(i, name)| if i == layer_index { None } else { Some(name) })
+            .collect();
+        let mut to_kill = Vec::new();
+        self.entities
+            .iter_with(&mut self.spawned_map_layer_metas)
+            .for_each(|(entity, layer)| {
+                if layer.layer_idx == layer_index {
+                    to_kill.push(entity);
+
+                    if let Some(tile_layer) = self.tile_layers.get(entity) {
+                        for tile in tile_layer.tiles.iter().flatten() {
+                            to_kill.push(*tile);
+                        }
+                    }
+                };
+
+                if layer.layer_idx > layer_count - layers_to_decrement {
+                    layer.layer_idx -= 1;
+                }
+            });
+        to_kill.into_iter().for_each(|ent| {
+            self.entities.kill(ent);
+        });
+    }
+    fn rename_layer(&mut self, layer_index: usize, name: String) {
+        self.spawned_map_meta.layer_names = self.spawned_map_meta
+            .layer_names
+            .clone()
+            .iter()
+            .cloned()
+            .enumerate()
+            .map(|(i, n)| if i == layer_index { name } else { n })
+            .collect();
+    }
+    fn move_element(&mut self, entity: Entity, position: &Vec2) {
+        let transform = self.transforms.get_mut(entity).unwrap();
+        transform.translation.x = position.x;
+        transform.translation.y = position.y;
+    }
+    fn delete_element(&mut self, entity: Entity) {
+        self.entities.kill(entity);
+    }
+    fn set_layer_tilemap(&mut self, layer_index: usize, tilemap: &Option<Handle<Atlas>>) {
+        if let Some((_, (tile_layer, _))) = self.entities
+            .iter_with((&mut self.tile_layers, &self.spawned_map_layer_metas))
+            .find(|x| x.1.1.layer_idx == layer_index)
+        {
+            if let Some(handle) = tilemap {
+                tile_layer.atlas = handle.clone();
+            } else {
+                tile_layer.atlas = default();
+            }
+        };
+    }
+    fn set_tile(&mut self, layer_index: usize, position: UVec2, tilemap_tile_index: &Option<usize>, tile_collision_kind: TileCollisionKind) {
+        if let Some((_, (tile_layer, _))) = self.entities
+            .iter_with((&mut self.tile_layers, &self.spawned_map_layer_metas))
+            .find(|x| x.1.1.layer_idx == layer_index)
+        {
+            if let Some(entity) = tile_layer.get(position) {
+                if let Some(idx) = tilemap_tile_index.as_ref() {
+                    self.tiles.get_mut(entity).unwrap().idx = *idx;
+
+                    // TODO: technically setting the collision to empty should be
+                    // equivalent to removing the component, but it isn't working like
+                    // that right now.
+                    if tile_collision_kind != TileCollisionKind::Empty {
+                        self.tile_collisions.insert(entity, tile_collision_kind);
+                    } else {
+                        self.tile_collisions.remove(entity);
+                    }
+                } else {
+                    self.entities.kill(entity);
+                    tile_layer.set(position, None);
+                }
+            } else if let Some(idx) = tilemap_tile_index {
+                let entity = self.entities.create();
+                tile_layer.set(position, Some(entity));
+                self.tiles.insert(
+                    entity,
+                    Tile {
+                        idx: *idx,
+                        ..default()
+                    },
+                );
+
+                // TODO: technically setting the collision to empty should be
+                // equivalent to removing the component, but it isn't workidesired_keyng like
+                // that right now.
+                if tile_collision_kind != TileCollisionKind::Empty {
+                    self.tile_collisions.insert(entity, tile_collision_kind);
+                } else {
+                    self.tile_collisions.remove(entity);
+                }
+            }
+
+            self.commands.add(move |mut collision_world: CollisionWorld| {
+                collision_world.update_tile(layer_index, position);
+            });
+        };
+    }
+    fn swap_layer(&mut self, layer_index: usize, is_downward: bool) {
+        let origin_layer_index = layer_index;
+        let other_layer_index = if is_downward { origin_layer_index + 1 } else { origin_layer_index - 1 };
+        let mut layer_names = self.spawned_map_meta.layer_names.to_vec();
+        layer_names.swap(origin_layer_index, other_layer_index);
+        self.spawned_map_meta.layer_names = layer_names.into_iter().collect();
+
+        for (_, (transform, layer_meta)) in
+            self.entities
+                .iter_with((&mut self.transforms, &mut self.spawned_map_layer_metas))
+        {
+            if layer_meta.layer_idx == origin_layer_index {
+                layer_meta.layer_idx = other_layer_index;
+                transform.translation.z = z_depth_for_map_layer(other_layer_index);
+            } else if layer_meta.layer_idx == other_layer_index {
+                layer_meta.layer_idx = origin_layer_index;
+                transform.translation.z = z_depth_for_map_layer(origin_layer_index);
+            }
+        }
+    }
+    fn rename_map(&mut self, name: String) {
+        self.spawned_map_meta.name = name.into();
+    }
+}
+
 fn handle_editor_input(
-    mut commands: Commands,
     player_inputs: Res<PlayerInputs>,
-    mut entities: ResMut<Entities>,
-    mut spawned_map_meta: ResMut<SpawnedMapMeta>,
-    mut element_handles: CompMut<ElementHandle>,
-    mut transforms: CompMut<Transform>,
-    mut spawned_map_layer_metas: CompMut<SpawnedMapLayerMeta>,
-    mut tile_layers: CompMut<TileLayer>,
-    mut tiles: CompMut<Tile>,
-    mut tile_collisions: CompMut<TileCollisionKind>,
+    mut map_interface: MapInterface
 ) {
     for player in &player_inputs.players {
         if let Some(editor_input) = &player.editor_input {
@@ -26,116 +212,28 @@ fn handle_editor_input(
                     translation,
                     layer,
                 } => {
-                    let ent = entities.create();
-                    element_handles.insert(ent, ElementHandle(handle.clone()));
-                    let z_depth = z_depth_for_map_layer(*layer as usize);
-                    transforms.insert(
-                        ent,
-                        Transform::from_translation(translation.extend(z_depth)),
-                    );
-                    spawned_map_layer_metas.insert(
-                        ent,
-                        SpawnedMapLayerMeta {
-                            layer_idx: *layer as usize,
-                        },
-                    );
+                    map_interface.create_element(handle, translation, *layer as usize);
                 }
                 EditorInput::CreateLayer { id } => {
-                    let ent = entities.create();
-                    let layer_idx = spawned_map_meta.layer_names.len();
-                    spawned_map_meta.layer_names = spawned_map_meta
-                        .layer_names
-                        .clone()
-                        .iter()
-                        .cloned()
-                        .chain([id.clone()])
-                        .collect();
-                    spawned_map_layer_metas.insert(ent, SpawnedMapLayerMeta { layer_idx });
-                    tile_layers.insert(
-                        ent,
-                        TileLayer::new(
-                            spawned_map_meta.grid_size,
-                            spawned_map_meta.tile_size,
-                            default(),
-                        ),
-                    );
-                    transforms.insert(
-                        ent,
-                        Transform::from_translation(Vec3::new(
-                            0.0,
-                            0.0,
-                            z_depth_for_map_layer(layer_idx),
-                        )),
-                    );
+                    map_interface.create_layer(id.clone());
                 }
                 EditorInput::DeleteLayer { layer } => {
-                    let layer = *layer as usize;
-                    let layer_count = spawned_map_meta.layer_names.len();
-                    let layers_to_decrement = layer_count - layer;
-                    spawned_map_meta.layer_names = spawned_map_meta
-                        .layer_names
-                        .clone()
-                        .iter()
-                        .cloned()
-                        .enumerate()
-                        .filter_map(|(i, name)| if i == layer { None } else { Some(name) })
-                        .collect();
-                    let mut to_kill = Vec::new();
-                    entities
-                        .iter_with(&mut spawned_map_layer_metas)
-                        .for_each(|(ent, l)| {
-                            if l.layer_idx == layer {
-                                to_kill.push(ent);
-
-                                if let Some(tile_layer) = tile_layers.get(ent) {
-                                    for tile in tile_layer.tiles.iter().flatten() {
-                                        to_kill.push(*tile);
-                                    }
-                                }
-                            };
-
-                            if l.layer_idx > layer_count - layers_to_decrement {
-                                l.layer_idx -= 1;
-                            }
-                        });
-                    to_kill.into_iter().for_each(|ent| {
-                        entities.kill(ent);
-                    });
+                    map_interface.delete_layer(*layer as usize);
                 }
                 EditorInput::RenameLayer {
                     layer,
                     name: new_name,
                 } => {
-                    let layer = *layer as usize;
-
-                    spawned_map_meta.layer_names = spawned_map_meta
-                        .layer_names
-                        .clone()
-                        .iter()
-                        .cloned()
-                        .enumerate()
-                        .map(|(i, name)| if i == layer { new_name.clone() } else { name })
-                        .collect();
+                    map_interface.rename_layer(*layer as usize, new_name.clone())
                 }
                 EditorInput::MoveEntity { entity, pos } => {
-                    let transform = transforms.get_mut(*entity).unwrap();
-                    transform.translation.x = pos.x;
-                    transform.translation.y = pos.y;
+                    map_interface.move_element(*entity, pos);
                 }
                 EditorInput::DeleteEntity { entity } => {
-                    entities.kill(*entity);
+                    map_interface.delete_element(*entity);
                 }
                 EditorInput::SetTilemap { layer, handle } => {
-                    if let Some((_ent, (tile_layer, _))) = entities
-                        .iter_with((&mut tile_layers, &spawned_map_layer_metas))
-                        .find(|x| x.1 .1.layer_idx == *layer as usize)
-                    {
-                        if let Some(handle) = handle {
-                            tile_layer.atlas = handle.clone();
-                        } else {
-                            tile_layer.atlas = default();
-                        }
-                    };
+                    map_interface.set_layer_tilemap(*layer as usize, handle);
                 }
                 EditorInput::SetTile {
                     layer,
@@ -143,76 +241,13 @@ fn handle_editor_input(
                     tilemap_tile_idx,
                     collision,
                 } => {
-                    let layer = *layer as usize;
-                    let pos = *pos;
-
-                    if let Some((_ent, (tile_layer, _))) = entities
-                        .iter_with((&mut tile_layers, &spawned_map_layer_metas))
-                        .find(|x| x.1 .1.layer_idx == layer)
-                    {
-                        if let Some(ent) = tile_layer.get(pos) {
-                            if let Some(idx) = tilemap_tile_idx.as_ref() {
-                                tiles.get_mut(ent).unwrap().idx = *idx;
-
-                                // TODO: technically setting the collision to empty should be
-                                // equivalent to removing the component, but it isn't working like
-                                // that right now.
-                                if *collision != TileCollisionKind::Empty {
-                                    tile_collisions.insert(ent, *collision);
-                                } else {
-                                    tile_collisions.remove(ent);
-                                }
-                            } else {
-                                entities.kill(ent);
-                                tile_layer.set(pos, None);
-                            }
-                        } else if let Some(idx) = tilemap_tile_idx {
-                            let ent = entities.create();
-                            tile_layer.set(pos, Some(ent));
-                            tiles.insert(
-                                ent,
-                                Tile {
-                                    idx: *idx,
-                                    ..default()
-                                },
-                            );
-
-                            // TODO: technically setting the collision to empty should be
-                            // equivalent to removing the component, but it isn't workidesired_keyng like
-                            // that right now.
-                            if *collision != TileCollisionKind::Empty {
-                                tile_collisions.insert(ent, *collision);
-                            } else {
-                                tile_collisions.remove(ent);
-                            }
-                        }
-
-                        commands.add(move |mut collision_world: CollisionWorld| {
-                            collision_world.update_tile(layer, pos);
-                        });
-                    };
+                    map_interface.set_tile(*layer as usize, *pos, tilemap_tile_idx, *collision);
                 }
                 EditorInput::MoveLayer { layer, down } => {
-                    let layer1 = *layer as usize;
-                    let layer2 = if *down { layer1 + 1 } else { layer1 - 1 };
-                    let mut layer_names = spawned_map_meta.layer_names.to_vec();
-                    layer_names.swap(layer1, layer2);
-                    spawned_map_meta.layer_names = layer_names.into_iter().collect();
-
-                    for (_ent, (transform, layer_meta)) in
-                        entities.iter_with((&mut transforms, &mut spawned_map_layer_metas))
-                    {
-                        if layer_meta.layer_idx == layer1 {
-                            layer_meta.layer_idx = layer2;
-                            transform.translation.z = z_depth_for_map_layer(layer2);
-                        } else if layer_meta.layer_idx == layer2 {
-                            layer_meta.layer_idx = layer1;
-                            transform.translation.z = z_depth_for_map_layer(layer1);
-                        }
-                    }
+                    map_interface.swap_layer(*layer as usize, *down)
                 }
                 EditorInput::RenameMap { name } => {
-                    spawned_map_meta.name = name.clone().into();
+                    map_interface.rename_map(name.clone());
                 }
             }
         }

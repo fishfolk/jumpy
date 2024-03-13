ca53ba98d747f3438728448dd82ec4d281e434b5
Max Whitehead <35712032+MaxCWhitehead@users.noreply.github.com>
2024-03-12 23:20:52 -0700

    feat(frame-time-diag): Add bones frame time diagnostics window. (#921)
    companion PR for: https://github.com/fishfolk/bones/pull/346
    
    frame time window re-added to bones, this enables viewing it.

--- src/debug.rs
+++ src/debug.rs
@@ -1,6 +1,7 @@
 //! Debug tools and menus.
 
 use crate::prelude::*;
+use bones_framework::debug::frame_time_diagnostics_plugin;
 
 #[cfg(not(target_arch = "wasm32"))]
 use bones_framework::networking::debug::network_debug_window;
@@ -8,7 +9,8 @@ use bones_framework::networking::debug::network_debug_window;
 pub fn game_plugin(game: &mut Game) {
     game.sessions
         .create(SessionNames::DEBUG)
-        .install_plugin(session_plugin);
+        .install_plugin(session_plugin)
+        .install_plugin(frame_time_diagnostics_plugin);
 }
 
 fn session_plugin(session: &mut Session) {
@@ -72,6 +74,19 @@ fn debug_menu(
                             session.restore(&mut snapshot);
                         }
                     }
+                }
+
+                let show_frame_time_window = &mut ctx
+                    .get_state::<bones_framework::debug::FrameTimeWindowState>()
+                    .open;
+                if ui
+                    .button(localization.get("frame-time-diagnostics"))
+                    .clicked()
+                {
+                    *show_frame_time_window = !*show_frame_time_window;
+                    ctx.set_state(bones_framework::debug::FrameTimeWindowState {
+                        open: *show_frame_time_window,
+                    });
                 }
 
                 // Show net diagnostics button

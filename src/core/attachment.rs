4b94d7f5a08e95e280e73e59bb416079f0cf37d1
Max Whitehead <max.blackllama.alt3@gmail.com>
2024-04-14 22:51:17 -0700

    feat: AttachmentEase component + Attachment metadata

--- src/core/attachment.rs
+++ src/core/attachment.rs
@@ -40,20 +40,109 @@ pub struct Attachment {
     pub offset_inherits_rotation: bool,
 }
 
+/// Component that animates attachment offset with easing
+#[derive(Clone, HasSchema, Debug, Default)]
+pub struct AttachmentEasing {
+    /// Initial offset of entity
+    pub initial_offset: Vec2,
+    /// Target offset after easing is complete
+    pub target_offset: Vec2,
+
+    /// Offset ease timer used to update progress of Ease
+    pub offset_ease_timer: Timer,
+
+    /// Ease
+    pub ease: Ease,
+}
+
+/// Metadata for attachment
+#[derive(HasSchema, Clone, Default)]
+#[type_data(metadata_asset("attachment"))]
+#[repr(C)]
+pub struct AttachmentMeta {
+    pub name: Ustr,
+    pub atlas: Handle<Atlas>,
+    pub offset: Vec2,
+    pub animation: AnimatedSprite,
+
+    /// Synchronize [`AtlasSprite`] animation with entity animation
+    pub sync_animation: bool,
+    /// Synchronize [`Sprite`] color with entity color
+    pub sync_color: bool,
+    /// Synchronize [`Sprite`] flip values from parent (both flip and offset)
+    pub sync_flip: bool,
+    /// Should attachment inherit parent's rotation (applied to offset and sprite)
+    pub offset_inherits_rotation: bool,
+
+    pub attachment_easing: Handle<AttachmentEasingMeta>,
+}
+
+impl AttachmentMeta {
+    /// Make [`Attachment`] from meta and entity
+    pub fn attachment(&self, entity: Entity, offset_z: f32) -> Attachment {
+        Attachment {
+            entity,
+            offset: self.offset.extend(offset_z),
+            sync_animation: self.sync_animation,
+            sync_color: self.sync_color,
+            sync_flip: self.sync_flip,
+            offset_inherits_rotation: self.offset_inherits_rotation,
+        }
+    }
+}
+
+/// Metadata for [`AttachmentEasing`]
+#[derive(HasSchema, Clone, Default)]
+#[type_data(metadata_asset("attachment_easing"))]
+#[repr(C)]
+pub struct AttachmentEasingMeta {
+    /// Amount offset is changed over time (target offset = initial_offset + delta_offset)
+    pub delta_offset: Vec2,
+    /// How long to apply ease function until delta_offset fully applied
+    pub offset_ease_duration: Duration,
+
+    pub ease: EaseMeta,
+}
+
+impl AttachmentEasing {
+    /// Ease progress with delta time
+    pub fn tick(&mut self, delta_time: Duration) {
+        self.offset_ease_timer.tick(delta_time);
+        self.ease.progress = self.offset_ease_timer.percent();
+    }
+    /// Compute offset from current progress
+    pub fn compute_offset(&self) -> Vec2 {
+        let progress = self.ease.output();
+        self.initial_offset.lerp(self.target_offset, progress)
+    }
+}
+
 /// System to update the transforms of entities with the [`Attachment`] component.
 pub fn update_attachments(
     time: Res<Time>,
     entities: Res<Entities>,
     mut sprites: CompMut<Sprite>,
     attachments: Comp<Attachment>,
+    mut attachment_easings: CompMut<AttachmentEasing>,
     invincibles: Comp<Invincibility>,
     mut transforms: CompMut<Transform>,
     mut atlas_sprites: CompMut<AtlasSprite>,
 ) {
-    for (ent, attachment) in entities.iter_with(&attachments) {
+    for (ent, (attachment, easing)) in
+        entities.iter_with((&attachments, &mut OptionalMut(&mut attachment_easings)))
+    {
         let Some(attached_transform) = transforms.get(attachment.entity).copied() else {
             continue;
         };
+
+        let mut offset = attachment.offset;
+
+        // If have a AttachmentEasing component, update offset
+        if let Some(easing) = easing {
+            easing.tick(time.delta());
+            offset = easing.compute_offset().extend(offset.z);
+        }
+
         let transform = transforms
             .get_mut(ent)
             .expect("Entities with `Attachment` component must also have a `Transform` component.");
@@ -65,8 +154,6 @@ pub fn update_attachments(
             transform.translation = attached_transform.translation;
             transform.scale = attached_transform.scale;
         }
-
-        let mut offset = attachment.offset;
 
         if attachment.sync_flip {
             if let Some((flip_x, flip_y)) = atlas_sprites

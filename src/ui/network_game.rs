34118bb6f5fc3d0dda26e230c33eb0e43703a264
dignifiedquire <me@dignifiedquire.com>
2024-05-15 13:50:53 +0200

    feat: use iroh networking

--- src/ui/network_game.rs
+++ src/ui/network_game.rs
@@ -346,7 +346,9 @@ pub fn network_game_menu(
                                     });
                                 });
 
-                                let (is_recreated, service_info) = lan::prepare_to_host(host_info, service_name);
+                                let (is_recreated, service_info) = RUNTIME.block_on(async {
+                                    lan::prepare_to_host(host_info, service_name).await
+                                });
                                 if is_recreated {
                                     *status = NetworkGameStatus::Idle;
                                 }
@@ -456,7 +458,8 @@ pub fn network_game_menu(
                                 .clicked()
                                 {
                                     *status = NetworkGameStatus::Searching;
-                                    online::start_search_for_game(matchmaking_server.clone(), *player_count);
+                                    let server = matchmaking_server.parse().expect("invalid server id");
+                                    online::start_search_for_game(server, *player_count);
                                 }
                             } else if *status == NetworkGameStatus::Searching {
                                 if let Some(online_socket) = online::update_search_for_game(&mut search_state) {

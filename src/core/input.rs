5163247464b6e589fc8512d5ceab80a059cd591d
Tekhnae Raav <tekhnaeraav@katharostech.com>
2026-01-28 14:35:18 -0600

    fix: update `Controls` trait impl for new bones
    This moves `get_control_source` out of the `Controls` trait implementation and removes the unused `update_controls` function from the trait.

--- src/core/input.rs
+++ src/core/input.rs
@@ -15,7 +15,11 @@ pub fn install(session: &mut SessionBuilder) {
 pub struct MatchInputs {
     pub players: [PlayerInput; MAX_PLAYERS as usize],
 }
-
+impl MatchInputs {
+    pub fn get_control_source(&self, player_idx: usize) -> Option<ControlSource> {
+        self.players.get(player_idx).unwrap().control_source
+    }
+}
 impl Default for MatchInputs {
     fn default() -> Self {
         Self {
@@ -24,23 +28,6 @@ impl Default for MatchInputs {
     }
 }
 impl Controls<'_, PlayerControl> for MatchInputs {
-    type ControlSource = ControlSource;
-    type ControlMapping = PlayerControlMapping;
-    type InputCollector = PlayerInputCollector;
-
-    fn update_controls(&mut self, collector: &mut PlayerInputCollector) {
-        (0..MAX_PLAYERS as usize).for_each(|i| {
-            let player_input = &mut self.players[i];
-            if let Some(source) = &player_input.control_source {
-                player_input.control = *collector.get_control(i, *source);
-            }
-        });
-    }
-
-    fn get_control_source(&self, player_idx: usize) -> Option<ControlSource> {
-        self.players.get(player_idx).unwrap().control_source
-    }
-
     fn get_control(&self, player_idx: usize) -> &PlayerControl {
         &self.players.get(player_idx).unwrap().control
     }
